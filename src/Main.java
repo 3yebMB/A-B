ac7b22ce62b3b32cacb21f1887360b3e28d89b31
3yebmb <3yebmb@gmail.com>
2019-01-30 14:18:53 +0500

    added catch of Exception which might be throw in converting string to integer

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,55 @@
-package PACKAGE_NAME;
+import java.io.*;
 
 public class Main {
+    public static void main(String[] args) {
+        String line;
+        String result;
+        String[] subStr = new String[2];
+
+        try {
+            FileInputStream fis = new FileInputStream("INPUT.TXT");
+            BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+
+            try {
+                line = br.readLine();
+                subStr = line.split(" ");
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    br.close();
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            result = Integer.toString(Integer.parseInt(subStr[0]) + Integer.parseInt(subStr[1]));
+
+            System.out.println(result);
+
+            FileOutputStream fos = new FileOutputStream("OUTPUT.TXT");
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
+
+            try {
+                bw.write(result);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            finally {
+                try {
+                    bw.close();
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
